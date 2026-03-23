9e5447009e817999886364564a697a86ccdb0e96
Kabuto-Mainer <snv.bor@gmail.com>
2026-03-23 13:34:23 +0300

    Рабоает %f

--- my_prinf.asm
+++ my_prinf.asm
@@ -1,7 +1,12 @@
 ;===================================================================
+default abs
+;===================================================================
 section .data
-    format db 'Poltorashka say %s %d times a week %d %d %d %d %d', 0x0
+    format db 'Poltorashka say %f %d times a week', 0x0
     string db 'Meow Meow', 0x0
+    float_value dd 0x4048F5C3 ;3.14
+    AMOUNT_STACK_ARG dq 0
+    TEN dd 10.0
 
 ;===================================================================
 section .text
@@ -12,15 +17,16 @@ _start:
     ; jmp end
 
 .real_start:
-
     mov rdi, format
-    mov rsi, string
-    mov rdx, 1000
-    mov rcx, 1
-    mov r8, 2
-    mov r9, 3
-    push 0x10f
-    push 4
+    mov rsi, 1
+    ; mov rsi, string
+    ; mov rdx, 1000
+    ; mov rcx, 1
+    ; mov r8, 2
+    ; mov r9, 3
+    movss xmm0, [float_value]
+    ; push 0x10f
+    ; push 4
 
     ; push rbx
     ; push rbp
@@ -36,7 +42,7 @@ _start:
 
 ;===================================================================
 ;rax - количество символов в буфере (стеке)
-;r14 - счетчик дополнительных аргументов (т.к. 5 передадутся через регистры, а остальные через стек)
+;r14 - счетчик дополнительных аргументов (т.к. 5 передадутся через sрегистры, а остальные через стек)
 ;r13 - буфер для обработки символов
 ;r12 - инициализируется rsp и является указателем на начало буфера, куда будет записываться строка, которая будет выводиться
 ;rdi - первый аргумент, являющийся адресом строки формата
@@ -67,7 +73,7 @@ _start:
     cmp r13b, '%'
     je .spec_plus_spec
 
-    call get_argument
+    call get_standard_argument
 ; ;!!! in r15 - argument to spec
 
     cmp r13b, 's'
@@ -105,6 +111,7 @@ section .text
 ;===================================================================
 .spec_string:
 
+;-------------------
 .next_spec_string:
     mov r13b, [r15]
     cmp r13b, 0
@@ -125,6 +132,7 @@ section .text
     mov rcx, 8
     rol r15b, 1
 
+;-------------------
 .next_spec_byte:
     push r15
 
@@ -154,53 +162,10 @@ section .text
 
 ;===================================================================
 .spec__d_ecimal:
-    push rbx
-    push rcx
-    push rdx
-
-    mov rbx, 10
-    xor rcx, rcx
-
-    test r15, r15
-    js .spec_d_convert
-    jz .spec_d_zero
-    jmp .spec_d_main_circle
-
-.spec_d_convert:
-    neg r15
-    mov rdx, '-'
-    push rdx
-    inc rcx
-
-.spec_d_main_circle:
-    test r15, r15
-    jz .end_spec_d
-
-    push rax
-    mov rax, r15
-    xor rdx, rdx
-    div rbx
-
-    add dl, '0'
-    mov r15, rax
-    pop rax
-
-    push rdx
-    inc rcx
-
-    jmp .spec_d_main_circle
-
-.end_spec_d:
-    pop rdx
-    mov [r12 + rax], dl
-    dec rax
-    loop .end_spec_d
-
-    pop rdx
-    pop rcx
-    pop rbx
+    call print_decimal
     jmp .next
 
+;-------------------
 .spec_d_zero:
     mov [r12 + rax], '0'
     dec rax
@@ -208,9 +173,94 @@ section .text
 
 ;===================================================================
 .spec__f_loat:
+    call get_avx_argument
+
+    push rbx
+    push rcx
+    push rdx
+    push r8
+    push r9
+    push r10
+    push r11
+
+    xor r8, r8
+    movd r8d, xmm8
+    mov r9d, r8d
+    shr r9d, 31
+    cmp r9d, 0
+    je .without_sign
+
+    mov byte [r12 + rax], '-'
+    dec rax
+
+    and r8d, ~0x80000000 ;clear sign bit
+
+.without_sign:
+
+    movd xmm8, r8d
+    mov r9d, r8d
+    and r9d, 0x7F800000
+    shr r9d, 23
+
+    mov r10d, r8d
+    and r10d, 0x007FFFFF
+
+    cmp r9d, 255
+    je .spec_f_inf_nan
+
+    cvttss2si r15, xmm8
+    mov r8, r15
+    call print_decimal
+
+    cvtsi2ss xmm9, r8
+    subss xmm8, xmm9
+
+    mov byte [r12 + rax], '.'
+    dec rax
+
+    mov rcx, 6
+
+.spec_f_circle:
+    mulss xmm8, [TEN]
+    cvttss2si r8, xmm8
+    cvtsi2ss xmm9, r8
+
+    add r8b, '0'
+    mov [r12 + rax], r8b
+    dec rax
+
+    subss xmm8, xmm9
+    loop .spec_f_circle
+
+    pop r11
+    pop r10
+    pop r9
+    pop r8
+    pop rdx
+    pop rcx
+    pop rbx
+
+    jmp .next
+
+.spec_f_inf_nan:
+    mov byte [r12 + rax], 'n'
+    mov byte [r12 + rax + 1], 'a'
+    mov byte [r12 + rax + 2], 'n'
+    sub rax, 3
+
+    pop r11
+    pop r10
+    pop r9
+    pop r8
+    pop rdx
+    pop rcx
+    pop rbx
+
+    jmp .next
 
 .spec__g_float:
 
+;===================================================================
 .spec__h_ex:
     push rbx
     push rcx
@@ -219,6 +269,7 @@ section .text
     mov rbx, 16
     xor rcx, rcx
 
+;-------------------
 .spec_h_main_circle:
     test r15, r15
     jz .end_spec_h
@@ -236,6 +287,7 @@ section .text
 
     jmp .spec_h_main_circle
 
+;-------------------
 .end_spec_h:
     pop rdx
 
@@ -255,6 +307,7 @@ section .text
     pop rbx
     jmp .next
 
+;===================================================================
 .spec_error:
 
 ;===================================================================
@@ -290,25 +343,19 @@ end:
 ;       r15 - argument
 ;       r14 ++
 ;===================================================================
-get_argument:
-    cmp r14, 5
+get_standard_argument:
+    cmp r14d, 5
     jae .get_from_stack
-    jmp [.jump_table + r14*8]
-
-section .rodata
-    align 8
-
-.jump_table:
-    dq   .arg_1
-    dq   .arg_2
-    dq   .arg_3
-    dq   .arg_4
-    dq   .arg_5
-
-section .text
+    jmp [.jump_table + r14d*8]
 
 .get_from_stack:
-    mov r15, [r12 + 8*(r14 - 4)]
+    push r14
+    mov r14, qword [AMOUNT_STACK_ARG]
+    mov r15, [r12 + 8*r14 + 8]
+    inc r14
+    mov qword [AMOUNT_STACK_ARG], r14
+    pop r14
+
     jmp .end
 
 .arg_1:
@@ -331,9 +378,99 @@ section .text
     mov r15, r9
 
 .end:
-    inc r14
+    inc r14d
     ret
 
+section .rodata
+    align 8
+
+.jump_table:
+    dq   .arg_1
+    dq   .arg_2
+    dq   .arg_3
+    dq   .arg_4
+    dq   .arg_5
+
+section .text
+
+
+;===================================================================
+; get_avx_argument
+;
+; get avx argument to xmm8
+; Entry:
+;       r14 - amount given argument
+;       r12 - not used stack pointer
+; Return:
+;       xmm8 - avx argument
+;===================================================================
+get_avx_argument:
+    dec r14
+    rol r14, 32
+
+    cmp r14d, 8
+    jae .get_from_stack
+    jmp [.jump_table + r14d*8]
+
+.arg_1:
+    movaps xmm8, xmm0
+    jmp .end
+
+.arg_2:
+    movaps xmm8, xmm1
+    jmp .end
+
+.arg_3:
+    movaps xmm8, xmm2
+    jmp .end
+
+.arg_4:
+    movaps xmm8, xmm3
+    jmp .end
+
+.arg_5:
+    movaps xmm8, xmm4
+    jmp .end
+
+.arg_6:
+    movaps xmm8, xmm5
+    jmp .end
+
+.arg_7:
+    movaps xmm8, xmm6
+    jmp .end
+
+.arg_8:
+    movaps xmm8, xmm7
+    jmp .end
+
+.get_from_stack:
+    push r14
+    mov r14, [AMOUNT_STACK_ARG]
+    movsd xmm8, [r12 + 8*r14 + 8]
+    inc r14
+    mov [AMOUNT_STACK_ARG], r14
+    pop r14
+
+.end:
+    inc r14d
+    rol r14, 32
+    ret
+
+section .rodata
+    align 8
+
+.jump_table:
+    dq   .arg_1
+    dq   .arg_2
+    dq   .arg_3
+    dq   .arg_4
+    dq   .arg_5
+    dq   .arg_6
+    dq   .arg_7
+    dq   .arg_8
+
+section .text
 
 ;===================================================================
 ; buffer_control
@@ -440,4 +577,69 @@ change_strait:
     pop rsi
     pop rdx
     pop rcx
+    ret
+
+
+;===================================================================
+;
+;===================================================================
+print_decimal:
+    push rcx
+    push rdx
+    push r8
+
+    mov r8, 10
+    xor rcx, rcx
+
+    test r15, r15
+    js .convert
+    jz .zero
+    jmp .main_circle
+
+.convert:
+    neg r15
+    mov rdx, '-'
+    push rdx
+    inc rcx
+
+.main_circle:
+    test r15, r15
+    jz .end
+
+    push rax
+    mov rax, r15
+    xor rdx, rdx
+    div r8
+
+    add dl, '0'
+    mov r15, rax
+    pop rax
+
+    push rdx
+    inc rcx
+
+    jmp .main_circle
+
+;-------------------
+.end:
+    pop rdx
+    mov [r12 + rax], dl
+    dec rax
+    loop .end
+
+    pop r8
+    pop rdx
+    pop rcx
+
+    ret
+
+;-------------------
+.zero:
+    mov [r12 + rax], '0'
+    dec rax
+
+    pop r8
+    pop rdx
+    pop rcx
+
     ret
